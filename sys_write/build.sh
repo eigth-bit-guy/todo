4c96dbb8a3889ca7aaf27c7020f0c0903c23c6f2
Samuel Henrique <henriquedossantosq8d@gmail.com>
2024-05-05 14:44:32 -0300

    fix: ajust build script

--- sys_write/build.sh
+++ sys_write/build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-echo "compiling..."
 
 nasm -f elf64 main.s
 ld main.o -o main
-echo "compilation finished and link too"
 
-echo "removing object file(s)"
 #clean
 rm -rf *.o
 
-echo "Ok"
